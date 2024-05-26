de2d3d6fed1a84651e09dd176fc292360aef8021
ArturYaniuk <pobedutelb@gmail.com>
2024-05-26 12:52:24 +0300

    Solved compile error

--- Source/Shooter/Private/ShooterPlayerController.cpp
+++ Source/Shooter/Private/ShooterPlayerController.cpp
@@ -2,4 +2,24 @@
 
 
 #include "ShooterPlayerController.h"
+#include "Blueprint/UserWidget.h"
 
+AShooterPlayerController::AShooterPlayerController()
+{
+
+}
+
+void AShooterPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HUDOverlayClass)
+	{
+		HUDOverlay = CreateWidget<UUserWidget>(this, HUDOverlayClass);
+		if (HUDOverlay)
+		{
+			HUDOverlay->AddToViewport();
+			HUDOverlay->SetVisibility(ESlateVisibility::Visible);
+		}
+	}
+}
--- Source/Shooter/Public/ShooterPlayerController.h
+++ Source/Shooter/Public/ShooterPlayerController.h
@@ -7,11 +7,25 @@
 #include "ShooterPlayerController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class SHOOTER_API AShooterPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	AShooterPlayerController();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+private:
+	/** Reference to the Overall HUD Overlay Blueprint Class */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Widgets, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class UUserWidget> HUDOverlayClass;
+
+	/** Variable to hold the HUD Overlay Widget after creating it */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Widgets, meta = (AllowPrivateAccess = "true"))
+	UUserWidget* HUDOverlay;
 };
